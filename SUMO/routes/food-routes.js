3ed504301e77dd755c04dd9d43183c6186959ec0
Matei-Dorian Năstase <75504638+matei-dorian@users.noreply.github.com>
2021-06-04 18:47:08 +0300

    Delete SUMO directory

--- SUMO/routes/food-routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const foodController = require('../controllers/food-controller');
-const router = express.Router();
-
-router.get('/', foodController.getFood);
-
-module.exports = router;
--- SUMO/routes/images-routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const imagesController = require('../controllers/images-controller');
-const router = express.Router();
-
-router.get('/', imagesController.getImages);
-
-module.exports = router;
